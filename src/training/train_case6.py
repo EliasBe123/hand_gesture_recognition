c18454677937e18130e8ea07c9040c65cddf1769
JosiasRoe <josias.roeland@gmail.com>
2026-05-18 10:34:23 +0200

    Added trained based on case 4

--- src/training/train_case6.py
+++ src/training/train_case6.py
@@ -5,8 +5,8 @@ import torch.nn as nn
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
-from src.utils.config_case2v2 import DEVICE, EPOCHS, LEARNING_RATE, FINETUNED_MODEL_PATH_CASE2, BEST_MODEL_PATH_CASE6
-from data.loader_case6_blackwhite import get_dataloaders
+from src.utils.config_case2v2 import BEST_MODEL_PATH_CASE6_1, DEVICE, EPOCHS, LEARNING_RATE, FINETUNED_MODEL_PATH_CASE2, BEST_MODEL_PATH_CASE6, BEST_MODEL_PATH_CASE4
+from src.data.loader_case6_blackwhite import get_dataloaders
 from src.models.cnn_case2v2 import HandGestureCNN_Case2
 
 def train_one_epoch(model, loader, criterion, optimizer, device):
@@ -49,7 +49,8 @@ def main():
     model = HandGestureCNN_Case2().to(device)
     
     # FIXME: borde använda Ellens case 4 istället
-    model.load_state_dict(torch.load(FINETUNED_MODEL_PATH_CASE2, map_location=device, weights_only=True))
+    #model.load_state_dict(torch.load(FINETUNED_MODEL_PATH_CASE2, map_location=device, weights_only=True))
+    model.load_state_dict(torch.load(BEST_MODEL_PATH_CASE4, map_location=device, weights_only=True))
 
     
     criterion = nn.CrossEntropyLoss()
@@ -71,7 +72,8 @@ def main():
         if val_acc > best_val_acc:
             best_val_acc = val_acc
             # 2. Save to the new path so original is untouched
-            torch.save(model.state_dict(), BEST_MODEL_PATH_CASE6)
+            #torch.save(model.state_dict(), BEST_MODEL_PATH_CASE6)
+            torch.save(model.state_dict(), BEST_MODEL_PATH_CASE6_1)
             print(f"  ✓ Saved best model (val_acc={val_acc:.4f})")
 
     print(f"\nTraining complete. Best val accuracy: {best_val_acc:.4f}")
